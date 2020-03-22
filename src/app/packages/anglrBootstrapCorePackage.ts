d791bc3a2e6d2764a91f76dbf400cc9bfc7d868a
kukjevov <kukjevov@gmail.com>
2020-03-22 10:14:39 +0100

    INT: added all subpackages

---
+++ src/app/packages/anglrBootstrapCorePackage.ts
@@ -0,0 +1,36 @@
+import {PackageBase} from '../packageBase';
+import {AnglrPackages} from '../interfaces/types';
+
+/**
+ * Represents package modifications for anglr bootstrap core
+ */
+export class AnglrBootstrapCorePackage extends PackageBase
+{
+    //######################### public properties #########################
+
+    /**
+     * Array of packages that is this package dependend on
+     */
+    public static get dependsOnPackages(): AnglrPackages[]
+    {
+        return ['@jscrpt/common', '@anglr/common', 'bootstrap', 'jquery'];
+    }
+
+    /**
+     * Array of packages that should be deleted when this one is not used
+     */
+    public static get cascadeDelete(): AnglrPackages[]
+    {
+        return [];
+    }
+
+    //######################### public methods #########################
+
+    /**
+     * Activates this package transformation
+     * @param activePackages Array of packages that are being activated
+     */
+    public activate(activePackages: AnglrPackages[]): void
+    {
+    }
+}
--- src/app/packages/index.ts
+++ src/app/packages/index.ts
@@ -1,13 +1,29 @@
 import {AnglrAnimationsPackage} from './anglrAnimationsPackage';
 import {AnglrAuthenticationPackage} from './anglrAuthenticationPackage';
 import {AnglrBootstrapPackage} from './anglrBootstrapPackage';
+import {AnglrBootstrapCorePackage} from './anglrBootstrapCorePackage';
+import {AnglrBootstrapDatetimepickerPackage} from './anglrBootstrapDatetimepickerPackage';
+import {AnglrBootstrapTypeaheadPackage} from './anglrBootstrapTypeaheadPackage';
 import {AnglrCommonPackage} from './anglrCommonPackage';
+import {AnglrCommonFormsPackage} from './anglrCommonFormsPackage';
+import {AnglrCommonHmrPackage} from './anglrCommonHmrPackage';
+import {AnglrCommonHotkeysPackage} from './anglrCommonHotkeysPackage';
+import {AnglrCommonMaterialPackage} from './anglrCommonMaterialPackage';
+import {AnglrCommonMomentPackage} from './anglrCommonMomentPackage';
+import {AnglrCommonNumeralPackage} from './anglrCommonNumeralPackage';
+import {AnglrCommonPositionsPackage} from './anglrCommonPositionsPackage';
+import {AnglrCommonRouterPackage} from './anglrCommonRouterPackage';
+import {AnglrCommonStorePackage} from './anglrCommonStorePackage';
+import {AnglrCommonStructuredLogPackage} from './anglrCommonStructuredLogPackage';
 import {AnglrErrorHandlingPackage} from './anglrErrorHandlingPackage';
 import {AnglrGridPackage} from './anglrGridPackage';
+import {AnglrGridMaterialPackage} from './anglrGridMaterialPackage';
 import {AnglrMdHelpPackage} from './anglrMdHelpPackage';
 import {AnglrNotificationsPackage} from './anglrNotificationsPackage';
 import {AnglrRestPackage} from './anglrRestPackage';
+import {AnglrRestStompjsPackage} from './anglrRestStompjsPackage';
 import {AnglrSelectPackage} from './anglrSelectPackage';
+import {AnglrSelectMaterialPackage} from './anglrSelectMaterialPackage';
 import {AngularCdkPackage} from './angularCdkPackage';
 import {AngularMaterialPackage} from './angularMaterialPackage';
 import {BootstrapDatetimepickerPackage} from './bootstrapDatetimepickerPackage';
@@ -35,13 +51,29 @@ export const packages: {[name: string]: PackageType} =
     '@anglr/animations': AnglrAnimationsPackage,
     '@anglr/authentication': AnglrAuthenticationPackage,
     '@anglr/bootstrap': AnglrBootstrapPackage,
+    '@anglr/bootstrap/core': AnglrBootstrapCorePackage,
+    '@anglr/bootstrap/datetimepicker': AnglrBootstrapDatetimepickerPackage,
+    '@anglr/bootstrap/typeahead': AnglrBootstrapTypeaheadPackage,
     '@anglr/common': AnglrCommonPackage,
+    '@anglr/common/forms': AnglrCommonFormsPackage,
+    '@anglr/common/hmr': AnglrCommonHmrPackage,
+    '@anglr/common/hotkeys': AnglrCommonHotkeysPackage,
+    '@anglr/common/material': AnglrCommonMaterialPackage,
+    '@anglr/common/moment': AnglrCommonMomentPackage,
+    '@anglr/common/numeral': AnglrCommonNumeralPackage,
+    '@anglr/common/positions': AnglrCommonPositionsPackage,
+    '@anglr/common/router': AnglrCommonRouterPackage,
+    '@anglr/common/store': AnglrCommonStorePackage,
+    '@anglr/common/structured-log': AnglrCommonStructuredLogPackage,
     '@anglr/error-handling': AnglrErrorHandlingPackage,
     '@anglr/grid': AnglrGridPackage,
+    '@anglr/grid/material': AnglrGridMaterialPackage,
     '@anglr/md-help': AnglrMdHelpPackage,
     '@anglr/notifications': AnglrNotificationsPackage,
     '@anglr/rest': AnglrRestPackage,
+    '@anglr/rest/stompjs': AnglrRestStompjsPackage,
     '@anglr/select': AnglrSelectPackage,
+    '@anglr/select/material': AnglrSelectMaterialPackage,
     '@jscrpt/common': JscrptCommonPackage,
     '@fortawesome/fontawesome-free': FontAwesomePackage,
     'moment': MomentPackage,
